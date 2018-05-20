38b469233f47005bb8f139077c9b1794e30390d4
Duncan Roberts <dliroberts@gmail.com>
2018-05-20 14:31:22 +0100

    refactored test code

--- src/test/java/asinoladro/db/BaseDaoTest.java
+++ src/test/java/asinoladro/db/BaseDaoTest.java
@@ -3,7 +3,6 @@ package asinoladro.db;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.rules.ExternalResource;
 
 import asinoladro.BankTransferExampleApplication;
 import asinoladro.BankTransferExampleConfiguration;
@@ -11,20 +10,17 @@ import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 
-public class DbSetupRule extends ExternalResource {
+public class BaseDaoTest {
 	@ClassRule
     public static final DropwizardAppRule<BankTransferExampleConfiguration> SUPPORT =
             new DropwizardAppRule<BankTransferExampleConfiguration>(
             		BankTransferExampleApplication.class,
-            		ResourceHelpers.resourceFilePath("config-test.yml")//,
-            		// use a randomly selected port
-//            		ConfigOverride.config("server.applicationConnectors[0].port", "0")
-            );
+            		ResourceHelpers.resourceFilePath("config-test.yml"));
     
     private Jdbi jdbi;
     
     @Before
-    protected void before() throws Throwable {
+    public void before() throws Throwable {
     		JdbiFactory factory = new JdbiFactory();
         jdbi = factory.build(SUPPORT.getEnvironment(),
         		SUPPORT.getConfiguration().getDataSourceFactory(), "database");
